6cdc2bcc7f3b8d0c55a5b66d7095193e6c3bf7a7
JohnnyJYWu <37619620+JohnnyJYWu@users.noreply.github.com>
2018-12-15 22:21:49 +0800

    test

---
+++ src/offer/Main.java
@@ -0,0 +1,14 @@
+package offer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		T66_movingCount test = new T66_movingCount();
+
+		int i = test.movingCount(5, 4, 4);
+		
+		System.out.println(i);
+	}
+
+}
